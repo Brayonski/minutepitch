17a44db397e62127722a3d933229e4ef18c2c391
EKibet <kibetedgar@gmail.com>
2018-09-07 19:50:27 +0300

    create posts schema

--- app/models.py
+++ app/models.py
@@ -12,4 +12,21 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+class Pitch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    '''
+    Pitch class represent the pitches posted by 
+    users. Timestamp is set to default and passsed datetime.utcnow--> function.
+    SQLAlchemy will set the field to the value of calling that function
+    and not the result of calling it without ()
+    The user_id field is initialized as a foreign key to user.id,
+    which means that it references an id value from the users table
+    '''
 
+    def __repr__(self):
+        return '<Pitch{}>'.format(self.body)
+
+        
