d22fec1fb4b51b9585d68978a2337375a8c42e59
EKibet <kibetedgar@gmail.com>
2018-09-07 19:50:58 +0300

    add SQLALCHEMY URI

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 
 class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yego:pass123@localhost/minutepitch'
+
     pass
 class ProdConfig(Config):
     pass
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yego:pass123@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yego:pass123@localhost/minutepitch'
     DEBUG = True
 
 config_options = {
