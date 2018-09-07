cb8897956d96a45cbfa69f5caceeebadcb33f5fa
EKibet <kibetedgar@gmail.com>
2018-09-07 22:14:09 +0300

    Create load_user

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
-from . import db
+from . import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
+    '''
+    UserMixin class that includes generic implementations
+    that are appropriate for most user model classes
+    '''
     __tablename__ = 'users'
     id =db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), index=True, unique = True)
@@ -9,15 +15,28 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Pitch', backref='author', lazy='dynamic')
 
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
+        '''
+    with these two methods in place, a user object is now 
+    able to do secure password verification
+    '''
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    '''
+    Flask-login keeps track of the logged in 
+    user by storing its unique identifier in Flask's
+    user session.
+    ''' 
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
-class Pitch(db.Model):
+class Pitch(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
