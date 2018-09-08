918d3643e6afecedcd3780a34466924c282eaad4
EKibet <kibetedgar@gmail.com>
2018-09-08 20:45:25 +0300

    Adds followers table

--- app/models.py
+++ app/models.py
@@ -5,7 +5,15 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from hashlib import md5
 from time import time
 import jwt
+from app import create_app
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
+)
+'''
+auxiliary table that has no data other than the foreign keys, I created it without an associated model class.
+'''
 
 class User(UserMixin, db.Model):
     '''
@@ -16,11 +24,27 @@ class User(UserMixin, db.Model):
     id =db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), index=True, unique = True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Pitch', backref='author', lazy='dynamic')
-    about_me = db.Column(db.String(140))
+    password_hash = db.Column(db.String(130))
+    pitch = db.relationship('Pitch', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(150))
     last_seen = db.Column(db.DateTime,default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
 
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -30,6 +54,7 @@ class User(UserMixin, db.Model):
     with these two methods in place, a user object is now 
     able to do secure password verification
     '''
+
     def avatar(self,size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
@@ -40,27 +65,13 @@ class User(UserMixin, db.Model):
     The verify_reset_password_token() is a static method, which means that it can be invoked directly from the class
     '''
 
-    def get_reset_password_token(self, expires_in=600):
-        return jwt.encode(
-        {'reset_password': self.id, 'exp': time() + expires_in},
-        app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+    def followed_posts(self):
+        followed = Pitch.query.join(
+            followers, (followers.c.followed_id == Pitch.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Pitch.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Pitch.timestamp.desc())
 
-    ''''
-    The get_reset_password_token() function generates a JWT token as a string.
-
-    '''
-    @staticmethod
-    def verify_reset_password_token(token):
-        try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
-                            algorithms=['HS256'])['reset_password']
-        except:
-            return
-        return User.query.get(id)
-
-        '''
-        If the token cannot be validated or is expired, an exception will be raised, and in that case I catch it to prevent the error, and then return None to the caller
-        '''
     def __repr__(self):
         return '<User {}>'.format(self.username)
     '''
@@ -73,12 +84,13 @@ class User(UserMixin, db.Model):
         return User.query.get(int(id))
 
 class Pitch(UserMixin, db.Model):
+    __tablename__= 'pitches'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     '''
-    Pitch class represent the pitches posted by 
+    Pitch class represent the pitches Pitched by 
     users. Timestamp is set to default and passsed datetime.utcnow--> function.
     SQLAlchemy will set the field to the value of calling that function
     and not the result of calling it without ()
