92cd3372382718dfa0d5f3c5316837bc373be046
EKibet <kibetedgar@gmail.com>
2018-09-08 15:22:54 +0300

    Adds email configurations

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yego:pass123@localhost/minutepitch'
     SECRET_KEY=os.environ.get('SECRET_KEY') or '1234'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
     pass
 class ProdConfig(Config):
     pass
