66900da3d23dcf4dbd0f8b5a6b25f897c2176fde
EKibet <kibetedgar@gmail.com>
2018-09-08 20:47:03 +0300

    updates migration command

--- manage.py
+++ manage.py
@@ -1,13 +1,14 @@
 from app import create_app,db
 from flask_script import Manager, Server
 from flask_migrate import Migrate,MigrateCommand
-from app.models import User
+from app.models import User,Pitch
 app = create_app('development')
 
-manager = Manager(app)
-migrate = Migrate(app, db)
 
+manager = Manager(app)
 manager.add_command('server',Server)
+
+migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 '''
 adds the databse instance and models to the shell session
