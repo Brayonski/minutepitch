c4c8f1a1c8ce4c5205a33812b383af817c0592df
EKibet <kibetedgar@gmail.com>
2018-09-08 20:43:56 +0300

    Routing for followers and following

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from flask import render_template,request,flash,redirect,url_for
 from . import main
 from flask_login import login_required,current_user
-from app.models import User
+from app.models import User,Pitch
 from datetime import datetime
 from app import db
-
+from .forms import PostForm
 
 @main.before_request
 def before_request():
@@ -29,6 +29,75 @@ def index():
     ]
 
     return render_template('index.html', title= title,posts=posts)
+
+
+
+@main.route('/index', methods=['GET', 'POST'])
+@login_required
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Pitch(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('main.index'))
+
+    posts = current_user.followed_posts().all()
+
+    return render_template("posts.html", title='Home Page', form=form,
+                           posts=posts)
+
+
+
+@main.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+@main.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username))
+    return redirect(url_for('user', username=username))
+# @login_required
+# def post():
+#     form = PostForm()
+#     if form.validate_on_submit():
+#         pitches = Pitch(body=form.post.data, author=current_user)
+#         db.session.add(post)
+#         db.session.commit()
+#         flash(_('Your post is now live!'))
+#         return redirect(url_for('index'))
+#     page = request.args.get('page', 1, type=int)
+#     pitches = current_user.followed_pitches().paginate(
+#         page, app.config['POSTS_PER_PAGE'], False)
+#     next_url = url_for('index', page=pitches.next_num) \
+#         if pitches.has_next else None
+#     prev_url = url_for('index', page=pitches.prev_num) \
+#         if pitches.has_prev else None
+#     return render_template('index.html', title=_('Home'), form=form,
+#                            pitches=pitches.items, next_url=next_url,
+#                            prev_url=prev_url)
 
 # @main.route('/user/<username>')
 # @login_required
