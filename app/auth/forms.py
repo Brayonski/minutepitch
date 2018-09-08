6b4a4f23dbf9982a3191cf0cd09fad17118be9f1
EKibet <kibetedgar@gmail.com>
2018-09-08 09:16:01 +0300

    Adds RegistrationForm class with its custom validators

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    '''
+    custom validators
+    '''
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username is already taken.')
+    
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email already exists')
+    '''
+    the two methods above issue database queries expecting there 
+    will be no results. In the event a result already exists,
+    a validation error is triggered by raising ValidationError
+    '''
