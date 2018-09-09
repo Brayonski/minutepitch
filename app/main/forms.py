8d2f736510e85326713e3bf5e9c4bb8996821c79
EKibet <kibetedgar@gmail.com>
2018-09-09 10:07:26 +0300

    Adds comments fn

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField, \
-    TextAreaField
+from wtforms import StringField,SubmitField, SelectField,TextAreaField
 from wtforms.validators import ValidationError, DataRequired
 
 
 class PostForm(FlaskForm):
     post = TextAreaField(('Say something'), validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Technology','Technology'),('Sports','Sports'),('Religion','Religion')])
+
     submit = SubmitField(('Submit'))
+
+class CommentForm(FlaskForm):
+    details = StringField('Write a comment',validators=[DataRequired()])
+    submit = SubmitField('Comment')
