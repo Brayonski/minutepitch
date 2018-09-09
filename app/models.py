1e8d730186c1f7fa2604a174097ee2065d5da298
EKibet <kibetedgar@gmail.com>
2018-09-09 10:08:58 +0300

    Updates pitches and users schema and adds comments

--- app/models.py
+++ app/models.py
@@ -26,25 +26,17 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(130))
     pitch = db.relationship('Pitch', backref='author', lazy='dynamic')
-    about_me = db.Column(db.String(150))
-    last_seen = db.Column(db.DateTime,default=datetime.utcnow)
-    followed = db.relationship(
-        'User', secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
-    def follow(self, user):
-        if not self.is_following(user):
-            self.followed.append(user)
+    bio = db.Column(db.String(255))
+    profile_pic = db.Column(db.String(255))
+    pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Comments', backref='user', lazy="dynamic")
 
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.followed.remove(user)
 
-    def is_following(self, user):
-        return self.followed.filter(
-            followers.c.followed_id == user.id).count() > 0
-    
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -54,24 +46,14 @@ class User(UserMixin, db.Model):
     with these two methods in place, a user object is now 
     able to do secure password verification
     '''
-
-    def avatar(self,size):
+    def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
-            digest, size)
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
     '''
     The new avatar() method of the User class returns the URL of the user's avatar image, 
     scaled to the requested size in pixels.For users that don't have an avatar registered, an "identicon" image will be generated
     The verify_reset_password_token() is a static method, which means that it can be invoked directly from the class
     '''
-
-    def followed_posts(self):
-        followed = Pitch.query.join(
-            followers, (followers.c.followed_id == Pitch.user_id)).filter(
-                followers.c.follower_id == self.id)
-        own = Pitch.query.filter_by(user_id=self.id)
-        return followed.union(own).order_by(Pitch.timestamp.desc())
-
     def __repr__(self):
         return '<User {}>'.format(self.username)
     '''
@@ -83,12 +65,17 @@ class User(UserMixin, db.Model):
     def load_user(id):
         return User.query.get(int(id))
 
-class Pitch(UserMixin, db.Model):
+class Pitch(db.Model):
     __tablename__= 'pitches'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
+    category = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    @classmethod
+    def retrieve_posts(cls,id):
+        pitches = Pitch.filter_by(id=id).all()
+        return pitches
     '''
     Pitch class represent the pitches Pitched by 
     users. Timestamp is set to default and passsed datetime.utcnow--> function.
@@ -102,3 +89,10 @@ class Pitch(UserMixin, db.Model):
         return '<Pitch{}>'.format(self.body)
 
         
+class Comments(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer,primary_key= True)
+    details = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
