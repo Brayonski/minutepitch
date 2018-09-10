f1063cd810b15470e62c18b0d496f6c7eb3a84c6
EKibet <kibetedgar@gmail.com>
2018-09-10 12:24:00 +0300

    Adds mail config

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class Config:
     #     # simple mde  configurations
     # SIMPLEMDE_JS_IIFE = True
     # SIMPLEMDE_USE_CDN = True
+    
 class ProdConfig(Config):
     pass
 class DevConfig(Config):
