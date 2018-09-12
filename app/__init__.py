c1027dc4c9519b267f2d1e0bb9fd1fd6276442b3
EKibet <kibetedgar@gmail.com>
2018-09-13 00:04:55 +0300

    Profile fn

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
 
 '''
 The auth.login is the function(or endpoint) name for the login views
@@ -33,6 +35,7 @@ def create_app(config_name):
     
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    configure_uploads(app,photos)
 
     #initializing the app with the flask extensions
     return app
