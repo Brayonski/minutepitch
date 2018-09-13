5b018daae52aa3b03d9626a9ec2bf7f41923ca42
EKibet <kibetedgar@gmail.com>
2018-09-13 10:06:25 +0300

    Change uri

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_COPPER_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass   
 
 class DevConfig(Config):
