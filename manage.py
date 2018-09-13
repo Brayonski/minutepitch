072d9913d66e8a26b77c247d96c71ec6072661ee
EKibet <kibetedgar@gmail.com>
2018-09-13 08:30:13 +0300

    polish up

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager, Server
 from flask_migrate import Migrate,MigrateCommand
 from app.models import User,Pitch
-app = create_app('development')
+app = create_app('production')
 
 
 manager = Manager(app)
@@ -13,12 +13,12 @@ manager.add_command('db', MigrateCommand)
 '''
 adds the databse instance and models to the shell session
 '''
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 @manager.shell
 def make_shell_context():
     return dict(app = app, db = db, User = User, Pitch =Pitch )
